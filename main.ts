7da973681b3181f4a49fd4cea691f243e98e27b2
anay116 <archie.sh1587@gmail.com>
2026-05-09 14:59:43 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,9 +11,6 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite2, 
     game.gameOver(true)
     game.setGameOverEffect(true, effects.smiles)
 })
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite3, location3) {
-    game.splash("WOW !")
-})
 let cat: Sprite = null
 game.splash("WELCOME TO PLATFORM RUNNER", "MAKE THE CAT ESCAPE FROM LAVA")
 music.play(music.createSong(hex`0078000408020300001c00010a006400f4016400000400000000000000000000000000050000040a0030003400051e2225292c05001c000f0a006400f4010a00000400000000000000000000000000000000021e0000000400051e2225292c10001400051e2225292c20002400051e2225292c06001c00010a006400f4016400000400000000000000000000000000000000021e0008000c00051e2225292c18001c00051e2225292c28002c00051e2225292c`), music.PlaybackMode.LoopingInBackground)
@@ -37,5 +34,6 @@ cat = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(cat, 100, 0)
+scene.cameraFollowSprite(cat)
 tiles.setCurrentTilemap(tilemap`level1`)
 cat.ay = 200
