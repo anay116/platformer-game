498c80170bcb8f84aa4c5c95ac93c82263eab850
anay116 <archie.sh1587@gmail.com>
2026-05-19 14:50:03 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,8 +13,8 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite2, 
 })
 let cat: Sprite = null
 game.splash("WELCOME TO PLATFORM RUNNER", "MAKE THE CAT ESCAPE FROM LAVA")
-music.play(music.createSong(hex`0078000408020300001c00010a006400f4016400000400000000000000000000000000050000040a0030003400051e2225292c05001c000f0a006400f4010a00000400000000000000000000000000000000021e0000000400051e2225292c10001400051e2225292c20002400051e2225292c06001c00010a006400f4016400000400000000000000000000000000000000021e0008000c00051e2225292c18001c00051e2225292c28002c00051e2225292c`), music.PlaybackMode.LoopingInBackground)
 scene.setBackgroundColor(9)
+music.play(music.createSong(hex`0078000408010205001c000f0a006400f4010a00000400000000000000000000000000000000021c0008000c00091e2022242527292a2c18001c00091e2022242527292a2c06001c00010a006400f4016400000400000000000000000000000000000000021c0000000400091e2022242527292a2c10001400091e2022242527292a2c`), music.PlaybackMode.LoopingInBackground)
 cat = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -33,7 +33,7 @@ cat = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(cat, 100, 0)
 scene.cameraFollowSprite(cat)
+controller.moveSprite(cat, 100, 0)
 tiles.setCurrentTilemap(tilemap`level1`)
 cat.ay = 200
